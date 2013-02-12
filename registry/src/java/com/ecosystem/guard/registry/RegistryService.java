1847e845b2e6ce5a044e93d8872f7cfbe71461a9
unknown <juancarlos.fernandez@sioux.safelayer.lan>
2013-02-12 18:06:02 +0100

    servlet compilado y empaquetado

---
+++ registry/src/java/com/ecosystem/guard/registry/RegistryService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 1999-2013 Safelayer Secure Communications, S.A.
+ *
+ * All rights reserved. No part of this source code may be reproduced,
+ * stored in a retrieval system, or transmitted, in any form or by any
+ * means, electronic, mechanical, photocopying, recording or otherwise,
+ * except as in the end-user license agreement, without the prior
+ * permission of the copyright owner.
+ */
+
+package com.ecosystem.guard.registry;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author juancarlos.fernandez
+ * @version $Revision$
+ */
+@SuppressWarnings("serial")
+@WebServlet(value = "/service", name = "registry-servlet")
+public class RegistryService extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
+	}
+}
