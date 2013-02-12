f95e363819ecd771a446397e2653b6329b992ad5
Juan Carlos <juanca@mobility>
2013-02-12 23:47:52 +0100

    persistencia funcionando

--- registry/src/java/com/ecosystem/guard/registry/RegistryService.java
+++ registry/src/java/com/ecosystem/guard/registry/RegistryService.java
@@ -1,22 +1,17 @@
-/*
- * Copyright (c) 1999-2013 Safelayer Secure Communications, S.A.
- *
- * All rights reserved. No part of this source code may be reproduced,
- * stored in a retrieval system, or transmitted, in any form or by any
- * means, electronic, mechanical, photocopying, recording or otherwise,
- * except as in the end-user license agreement, without the prior
- * permission of the copyright owner.
- */
-
 package com.ecosystem.guard.registry;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.ecosystem.guard.domain.dao.AccountInfo;
 
 /**
  * 
@@ -26,8 +21,27 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 @WebServlet(value = "/service", name = "registry-servlet")
 public class RegistryService extends HttpServlet {
+	
+	@PersistenceUnit(name="EcosystemGuard")
+	private EntityManagerFactory entityManagerFactory;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		try {
+			AccountInfo accInfo = new AccountInfo();
+			accInfo.setUsername("jc@gmail.com");
+			accInfo.setPassword("AAAAAAAAAAAA=");
+			accInfo.setTelephoneNumber("987676663");
+			accInfo.setRecoverMail("otro@gmail.com");
+			entityManager.persist(accInfo);
+			entityManager.getTransaction().commit();
+			response.getWriter().write("CORRECTO");
+		} catch( Exception e ) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace(response.getWriter());
+		}
+		response.getWriter().flush();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
