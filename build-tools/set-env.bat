ae4e5c91d1de74ef86e39cc78288ebecb6a64351
masterjc <juancarlos.fernandezj@gmail.com>
2013-03-12 13:58:24 +0100

    Nueva aplicacion EcosystemGuard App

--- build-tools/set-env.bat
+++ build-tools/set-env.bat
@@ -1,5 +1,6 @@
 @echo on
-set PROJECT_DIR=F:\
+set PROJECT_DIR=F:\ecosystemguard\ecosystemguard
 set JBOSS_DEPLOYMENT_DIR=C:\Software\jboss-as-7.1.1.Final\standalone\deployments
+set TOMCAT_DEPLOYMENT_DIR=C:\Software\apache-tomcat-6.0.36\webapps
 set HOST_DEPLOYMENT_DIR=C:\Software\ecosystemguard
 set JAVA_SEPARATOR=;
