ff4c8609f704d8451a44652e7440fd0bd0a7b0ab
masterjc <juancarlos.fernandezj@gmail.com>
2013-04-11 13:52:36 +0200

    Tests para tomar fotos acabado

--- test/src/java/com/ecosystem/guard/test/RecordVideoTest.java
+++ test/src/java/com/ecosystem/guard/test/RecordVideoTest.java
@@ -4,9 +4,13 @@ import java.io.FileOutputStream;
 
 import org.junit.Test;
 
+import com.ecosystem.guard.camera.impl.H264Resolution;
+import com.ecosystem.guard.camera.impl.VideoCodec;
+import com.ecosystem.guard.camera.impl.VideoContainer;
 import com.ecosystem.guard.common.StreamServiceRequestor;
 import com.ecosystem.guard.domain.Credentials;
-import com.ecosystem.guard.domain.service.host.RecordVideoRequest;
+import com.ecosystem.guard.domain.service.app.RecordVideoRequest;
+import com.ecosystem.guard.domain.service.app.VideoConfiguration;
 
 public class RecordVideoTest {
 	@Test
@@ -14,6 +18,12 @@ public class RecordVideoTest {
 		String url = "http://hb:8080/ecosystemguard-app/recordvideo";
 		RecordVideoRequest request = new RecordVideoRequest();
 		request.setCredentials(new Credentials("juancarlos.fernandezj@gmail.com", "demodemo"));
+		request.setLength(10);
+		VideoConfiguration config = new VideoConfiguration();
+		config.setCodec(VideoCodec.H264.name());
+		config.setContainer(VideoContainer.MP4.name());
+		config.setResolution(H264Resolution.HVGA.name());
+		request.setVideoConfiguration(config);
 		FileOutputStream video = new FileOutputStream("testvideo.mp4");
 		StreamServiceRequestor.sendRequest(request, RecordVideoRequest.class, url, video);
 	}
--- test/src/java/com/ecosystem/guard/test/TakePictureTest.java
+++ test/src/java/com/ecosystem/guard/test/TakePictureTest.java
@@ -1,6 +1,32 @@
 package com.ecosystem.guard.test;
 
+import java.io.FileOutputStream;
 
+import org.junit.Test;
+
+import com.ecosystem.guard.camera.impl.PictureContainer;
+import com.ecosystem.guard.camera.impl.PictureResolution;
+import com.ecosystem.guard.common.StreamServiceRequestor;
+import com.ecosystem.guard.domain.Credentials;
+import com.ecosystem.guard.domain.service.app.PictureConfiguration;
+import com.ecosystem.guard.domain.service.app.TakePictureRequest;
+
+/**
+ * 
+ * @author juancarlos.fernandez
+ * @version $Revision$
+ */
 public class TakePictureTest {
-	
+	@Test
+	public void takeDefaultPicture() throws Exception {
+		String url = "http://hb:8080/ecosystemguard-app/takepicture";
+		TakePictureRequest request = new TakePictureRequest();
+		request.setCredentials(new Credentials("juancarlos.fernandezj@gmail.com", "demodemo"));
+		PictureConfiguration pictureConfiguration = new PictureConfiguration();
+		pictureConfiguration.setContainer(PictureContainer.JPEG.name());
+		pictureConfiguration.setResolution(PictureResolution.SVGA.name());
+		request.setPictureConfiguration(pictureConfiguration);
+		FileOutputStream video = new FileOutputStream("testvideo.jpg");
+		StreamServiceRequestor.sendRequest(request, TakePictureRequest.class, url, video);
+	}
 }
