374311c70b789ac2e638d10a661f68783c4bce69
masterjc <juancarlos.fernandezj@gmail.com>
2013-04-24 16:38:07 +0200

    Cliente de EcosystemGuard por linea de comandos para PC

---
+++ client/pc/src/java/com/ecosystem/guard/pc/Client.java
@@ -0,0 +1,11 @@
+package com.ecosystem.guard.pc;
+
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String[] args) throws Exception {
+		System.out.println("EcosystemGuard PC Client");
+		System.out.println("************************");
+	}
+
+}
